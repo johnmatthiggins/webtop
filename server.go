15ef986dad615e8cd50138f35c1f4db7536cbc16
John Higgins <johnmatthiggins@gmail.com>
2025-05-17 14:25:44 -0700

    switched up port

--- server.go
+++ server.go
@@ -36,5 +36,5 @@ func home(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", home)
 	fmt.Println("...STARTING SERVER...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8880", nil))
 }
